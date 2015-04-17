283e4f3fb4984392fa1ec976635e07068c2c30eb
mucximilian <mucximilian@gmail.com>
2015-04-17 12:32:03 +0200

    Polygon selection scripts added

---
+++ sql/query_select_tags_and_style_polygon.sql
@@ -0,0 +1,27 @@
+﻿SELECT
+	f.id,
+	f.tags,
+	f.z_order,
+	s.brush,
+	s.brush_size,
+	s.color,
+	s.opacity,
+	s.dynamics,
+	i.image,
+	i.opacity
+FROM 
+	feature f
+JOIN	
+	style s 
+ON (
+	f.style = s.id
+)
+JOIN
+	image i
+ON (
+	f.style = i.style
+)
+WHERE f.geometry = 3
+AND f.zoom_max <= 12
+AND f.zoom_min >= 12
+ORDER BY f.z_order DESC
