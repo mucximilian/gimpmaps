39da28579745f22def73024a4c02a40e66f57ae3
mucximilian <mucximilian@gmail.com>
2015-04-18 20:19:05 +0200

    SQL scripts renamed

--- sql/call_function_get_unclipped_svg_tile.sql
+++ sql/call_function_get_unclipped_svg_tile.sql
@@ -1,19 +1,11 @@
-﻿SELECT * FROM (
-	SELECT * 
-	FROM get_unclipped_svg_tile(
-		1271912.15067,
-		6124746.20243,
-		1281696.09029,
-		6114962.26281,
-		12,
-		256
-	) AS tile (
-		id bigint,
-		highway text,
-		route text,
-		svg text
-	)
-) t
+﻿SELECT * FROM get_unclipped_svg_tile(
+	1271912.15067,
+	6124746.20243,
+	1281696.09029,
+	6114962.26281,
+	12,
+	256
+)
 WHERE
 	highway = 'motorway' OR
 	highway = 'motorway_link' OR
---
+++ sql/call_function_get_unclipped_svg_tile_subquery.sql
@@ -0,0 +1,22 @@
+﻿SELECT * FROM (
+	SELECT * 
+	FROM get_unclipped_svg_tile(
+		1271912.15067,
+		6124746.20243,
+		1281696.09029,
+		6114962.26281,
+		12,
+		256
+	) AS tile (
+		id bigint,
+		highway text,
+		route text,
+		svg text
+	)
+) t
+WHERE
+	highway = 'motorway' OR
+	highway = 'motorway_link' OR
+	highway = 'primary' OR 
+	route = 'road' OR 
+	highway = 'secondary'
