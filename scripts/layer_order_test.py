214959fc47e70b19330067ab5ccc121534be279d
mucximilian <mucximilian@gmail.com>
2015-04-22 15:04:22 +0200

    Layer groups test

--- scripts/layer_order_test.py
+++ scripts/layer_order_test.py
@@ -11,9 +11,11 @@ def run():
     image = pdb.gimp_image_new(width, height, RGB)
     pdb.gimp_context_set_background((255,255,255,255))
     
-    parent = pdb.gimp_layer_group_new(image)
+    group_1 = pdb.gimp_layer_group_new(image)
+    group_2 = pdb.gimp_layer_group_new(image)
     
-    pdb.gimp_image_insert_layer(image, parent, None, 0)         
+    pdb.gimp_image_insert_layer(image, group_1, None, 0)         
+    pdb.gimp_image_insert_layer(image, group_2, None, 1)         
 
     # Adding layer layers
     layer_1 = pdb.gimp_layer_new(
@@ -25,7 +27,7 @@ def run():
         100,
         NORMAL_MODE
     )
-    pdb.gimp_image_insert_layer(image, layer_1, parent, 0)
+    pdb.gimp_image_insert_layer(image, layer_1, group_1, 0)
     
     layer_2 = pdb.gimp_layer_new(
         image,
@@ -36,7 +38,7 @@ def run():
         100,
         NORMAL_MODE
     )
-    pdb.gimp_image_insert_layer(image, layer_2, parent, 1)
+    pdb.gimp_image_insert_layer(image, layer_2, group_1, 1)
     
     layer_3 = pdb.gimp_layer_new(
         image,
@@ -47,7 +49,7 @@ def run():
         100,
         NORMAL_MODE
     )
-    pdb.gimp_image_insert_layer(image, layer_3, parent, 2)
+    pdb.gimp_image_insert_layer(image, layer_3, group_1, 2)
     
     layer_4 = pdb.gimp_layer_new(
         image,
@@ -58,7 +60,7 @@ def run():
         100,
         NORMAL_MODE
     )
-    pdb.gimp_image_insert_layer(image, layer_4, parent, 3)
+    pdb.gimp_image_insert_layer(image, layer_4, group_1, 3)
     
     layer_5 = pdb.gimp_layer_new(
         image,
@@ -69,7 +71,40 @@ def run():
         100,
         NORMAL_MODE
     )
-    pdb.gimp_image_insert_layer(image, layer_5, parent, 4)
+    pdb.gimp_image_insert_layer(image, layer_5, group_1, 4)
+    
+    layer_6 = pdb.gimp_layer_new(
+        image,
+        width,
+        height,
+        RGBA_IMAGE,
+        "layer_6",
+        100,
+        NORMAL_MODE
+    )
+    pdb.gimp_image_insert_layer(image, layer_6, group_2, 0)
+    
+    layer_7 = pdb.gimp_layer_new(
+        image,
+        width,
+        height,
+        RGBA_IMAGE,
+        "layer_7",
+        100,
+        NORMAL_MODE
+    )
+    pdb.gimp_image_insert_layer(image, layer_7, group_2, 1)
+    
+    layer_8 = pdb.gimp_layer_new(
+        image,
+        width,
+        height,
+        RGBA_IMAGE,
+        "layer_8",
+        100,
+        NORMAL_MODE
+    )
+    pdb.gimp_image_insert_layer(image, layer_8, group_2, 2)
 
 
     # Saving
@@ -89,7 +124,7 @@ def run():
     pdb.gimp_xcf_save(
         0,
         image,
-        parent,
+        image,
         out_path_xcf,
         out_path_xcf
     )
